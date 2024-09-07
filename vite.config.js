c7f2d2d1e413e900bd2e61d747ec6613020d41cb
sha5b <ned.tabulov@gmail.com>
2024-09-07 14:14:38 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+	  port: process.env.PORT || 3000,  // Use the port provided by Railway
+	  host: '0.0.0.0',  // Make sure the app is accessible externally
+	}
 });
